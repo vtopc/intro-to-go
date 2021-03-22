1124059a295a381c0bf8e793337e4d4144e9db66
vtopc <32271530+vtopc@users.noreply.github.com>
2021-03-22 16:28:49 +0200

    added test cases

--- http/server/handler_test.go
+++ http/server/handler_test.go
@@ -13,45 +13,125 @@ import (
 )
 
 func TestTimeoutMiddleware(t *testing.T) {
-	wrappedHandler := TimeoutMiddleware(http.HandlerFunc(asyncSlowHandler), 5*time.Second)
-	srv := NewServer(wrappedHandler)
-
-	// start http server in background since it's blocking:
-	go func() {
-		log.Print("starting http server on ", srv.Addr)
-		log.Print(srv.ListenAndServe())
-	}()
-
-	// workaround to start srv:
-	runtime.Gosched()
-
-	// client := &http.Client{
-	// 	Timeout: 20 * time.Second,
-	// }
-
-	transport := &http.Transport{
-		ResponseHeaderTimeout: 60 * time.Second,
-
-		// defaults:
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).DialContext,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
+	tests := map[string]struct {
+		timeout        time.Duration
+		expectedStatus int
+	}{
+		"timeout": {
+			timeout:        5 * time.Second,
+			expectedStatus: http.StatusGatewayTimeout,
+		},
+		"no_timeout": {
+			timeout:        60 * time.Second,
+			expectedStatus: http.StatusOK,
+		},
 	}
 
-	req, err := http.NewRequest(http.MethodGet, "http://"+srv.Addr+"/foo", nil)
-	require.NoError(t, err)
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			wrappedHandler := TimeoutMiddleware(http.HandlerFunc(asyncSlowHandler), tt.timeout)
+			srv := NewServer(wrappedHandler)
+			defer srv.Close()
 
-	resp, err := transport.RoundTrip(req)
-	t.Logf("resp: %+v", resp)
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusGatewayTimeout, resp.StatusCode)
+			// start http server in background since it's blocking:
+			go func() {
+				log.Print("starting http server on ", srv.Addr)
+				log.Print(srv.ListenAndServe())
+			}()
+
+			// workaround to start srv:
+			runtime.Gosched()
+
+			// client := &http.Client{
+			// 	Timeout: 20 * time.Second,
+			// }
+
+			transport := &http.Transport{
+				ResponseHeaderTimeout: 60 * time.Second,
+
+				// defaults:
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+				}).DialContext,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			}
+
+			req, err := http.NewRequest(http.MethodGet, "http://"+srv.Addr+"/foo", nil)
+			require.NoError(t, err)
+
+			resp, err := transport.RoundTrip(req)
+			t.Logf("resp: %+v", resp)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expectedStatus, resp.StatusCode)
+		})
+	}
+}
+
+func TestTimeoutHandler(t *testing.T) {
+	tests := map[string]struct {
+		timeout        time.Duration
+		expectedStatus int
+	}{
+		"timeout": {
+			timeout:        5 * time.Second,
+			expectedStatus: http.StatusServiceUnavailable,
+		},
+		"no_timeout": {
+			timeout:        60 * time.Second,
+			expectedStatus: http.StatusOK,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			wrappedHandler := http.TimeoutHandler(http.HandlerFunc(slowHandler), tt.timeout, "timeout")
+			srv := NewServer(wrappedHandler)
+			defer srv.Close()
+
+			// start http server in background since it's blocking:
+			go func() {
+				log.Print("starting http server on ", srv.Addr)
+				log.Print(srv.ListenAndServe())
+			}()
+
+			// workaround to start srv:
+			runtime.Gosched()
+
+			// client := &http.Client{
+			// 	Timeout: 20 * time.Second,
+			// }
+
+			transport := &http.Transport{
+				ResponseHeaderTimeout: 60 * time.Second,
+
+				// defaults:
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+				}).DialContext,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			}
+
+			req, err := http.NewRequest(http.MethodGet, "http://"+srv.Addr+"/foo", nil)
+			require.NoError(t, err)
+
+			resp, err := transport.RoundTrip(req)
+			t.Logf("resp: %+v", resp)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expectedStatus, resp.StatusCode)
+		})
+	}
 }
 
 func asyncSlowHandler(w http.ResponseWriter, r *http.Request) {
