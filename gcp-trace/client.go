9af612ac4a069dd4adf54732e26f3e255b51b3ba
vtopc <32271530+vtopc@users.noreply.github.com>
2021-05-20 13:02:50 +0300

    added BenchmarkWithoutTrace

--- gcp-trace/client.go
+++ gcp-trace/client.go
@@ -1,18 +1,10 @@
 package main
 
-import (
-	"net/http"
-	"time"
-
-	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
-	"go.opencensus.io/plugin/ochttp"
-)
-
-var tracedClient = &http.Client{
-	Timeout: 30 * time.Second,
-	Transport: &ochttp.Transport{
-		// Use Google Cloud propagation format.
-		Propagation:    &propagation.HTTPFormat{},
-		FormatSpanName: formatSpanName,
-	},
-}
+// var tracedClient = &http.Client{
+// 	Timeout: 30 * time.Second,
+// 	Transport: &ochttp.Transport{
+// 		// Use Google Cloud propagation format.
+// 		Propagation:    &propagation.HTTPFormat{},
+// 		FormatSpanName: formatSpanName,
+// 	},
+// }
--- gcp-trace/go.mod
+++ gcp-trace/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.6
+	github.com/stretchr/testify v1.6.1
 	go.opencensus.io v0.23.0
 )
