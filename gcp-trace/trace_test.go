3ab8298280f98964f7df0ca330828b793c33cbc5
vtopc <32271530+vtopc@users.noreply.github.com>
2021-05-20 16:50:01 +0300

    added BenchmarkWithTrace

--- gcp-trace/trace_test.go
+++ gcp-trace/trace_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"gcp-trace/httpserver"
+	"gcp-trace/ltrace"
 
 	"github.com/stretchr/testify/require"
 )
@@ -21,6 +22,34 @@ var client = &http.Client{
 
 func BenchmarkWithoutTrace(b *testing.B) {
 	router := httpserver.NewRouter(false)
+	srv := httpserver.NewServer(addr, router)
+	go func() {
+		b.Logf("starting http server on %s", addr)
+		err := srv.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			b.Logf("http srv error: %s", err)
+		}
+	}()
+	defer func() {
+		_ = srv.Shutdown(context.Background())
+	}()
+
+	// workaround for srv to start:
+	runtime.Gosched()
+
+	req, err := http.NewRequest(http.MethodGet, "http://"+addr+"/foo", nil)
+	require.NoError(b, err)
+
+	for n := 0; n < b.N; n++ {
+		_, err := client.Do(req)
+		require.NoError(b, err)
+	}
+}
+
+func BenchmarkWithTrace(b *testing.B) {
+	ltrace.RegisterTrace()
+
+	router := ltrace.HTTPHandlerWrapper(httpserver.NewRouter(true))
 	srv := httpserver.NewServer(addr, router)
 	go func() {
 		b.Logf("starting http server on %s", addr)
