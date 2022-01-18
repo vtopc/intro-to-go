ef8ef9121ab994edfdc6915758b0114c7244e6ca
vtopc <32271530+vtopc@users.noreply.github.com>
2022-01-18 21:27:58 +0200

    added bench

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -7,7 +7,7 @@ import (
 
 const count = 100
 
-func BenchmarkIndexSlice(b *testing.B) {
+func BenchmarkSliceIndex(b *testing.B) {
 	var s []S
 
 	for n := 0; n < b.N; n++ {
@@ -21,7 +21,7 @@ func BenchmarkIndexSlice(b *testing.B) {
 	_ = s
 }
 
-func BenchmarkAppendSlice(b *testing.B) {
+func BenchmarkSliceAppend(b *testing.B) {
 	var s []S
 
 	for n := 0; n < b.N; n++ {
@@ -30,6 +30,47 @@ func BenchmarkAppendSlice(b *testing.B) {
 		for i := 0; i < count; i++ {
 			s = append(s, newStruct())
 		}
+	}
+
+	_ = s
+}
+
+func BenchmarkAllocStructLiteral(b *testing.B) {
+	var s S
+
+	for n := 0; n < b.N; n++ {
+		s = S{
+			A: 1,
+			B: 2,
+			C: 3,
+			D: 4,
+			E: 5,
+			F: 6,
+			G: 7,
+			H: 8,
+			J: 9,
+			K: 10,
+		}
+	}
+
+	_ = s
+}
+
+func BenchmarkAllocStructByFields(b *testing.B) {
+	var s S
+
+	for n := 0; n < b.N; n++ {
+		var s S
+		s.A = 1
+		s.B = 2
+		s.C = 3
+		s.D = 4
+		s.E = 5
+		s.F = 6
+		s.G = 7
+		s.H = 8
+		s.J = 9
+		s.K = 10
 	}
 
 	_ = s
