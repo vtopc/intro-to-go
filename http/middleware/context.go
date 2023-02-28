4ab32e90accc4f22a46deacaac8a49fd99fc66f4
vtopc <32271530+vtopc@users.noreply.github.com>
2023-02-28 20:17:11 +0200

    added doc

--- http/middleware/context.go
+++ http/middleware/context.go
@@ -11,6 +11,7 @@ func CtxWithUserID(ctx context.Context, userID string) context.Context {
 	return context.WithValue(ctx, userIDContextKey, userID)
 }
 
+// ReqWithCtx updates provided http.Request with Context
 func ReqWithCtx(ctx context.Context, r *http.Request) {
 	req := r.Clone(ctx)
 	*r = *req
