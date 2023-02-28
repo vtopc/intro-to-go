be243fd45304e635ee6f7d191fa643fe3285d010
vtopc <32271530+vtopc@users.noreply.github.com>
2023-02-28 20:18:48 +0200

    added comment

--- http/middleware/context.go
+++ http/middleware/context.go
@@ -13,6 +13,10 @@ func CtxWithUserID(ctx context.Context, userID string) context.Context {
 
 // ReqWithCtx updates provided http.Request with Context
 func ReqWithCtx(ctx context.Context, r *http.Request) {
+	// To change the context of a request, such as an incoming request you
+	// want to modify before sending back out, use Request.Clone.
+	// https://pkg.go.dev/net/http@go1.19.6#Request.WithContext
 	req := r.Clone(ctx)
+
 	*r = *req
 }
