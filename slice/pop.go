1cfe03b5e41ecc67a7c1ad5c419ef0b1ff579f12
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-10-08 17:41:43 +0300

    check

--- slice/pop.go
+++ slice/pop.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	s := []int{1, 2, 3, 4, 5}
+	s2 := s
 	for {
-		if len(s) == 0 {
+		if len(s)-1 == 0 {
 			break
 		}
 
@@ -15,4 +16,8 @@ func main() {
 		s = s[1:] // pop first element
 		fmt.Printf("v: %v; s: %v; cap(%d); len(%d) \n", v, s, cap(s), len(s))
 	}
+
+	s[0] = 100500
+
+	fmt.Println("s2: ", s2)
 }
