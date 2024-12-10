9bda9bc845ad219217d4068b6dc7dc29ee204cba
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2024-12-10 12:56:12 +0200

    pointers

--- benchmarks/ranges/main.go
+++ benchmarks/ranges/main.go
@@ -22,3 +22,21 @@ func sumValuesByIndex(input []S) int {
 
 	return sum
 }
+
+func sumPointerElements(input []*S) int {
+	sum := 0
+	for _, elem := range input {
+		sum += elem.i
+	}
+
+	return sum
+}
+
+func sumPointersByIndex(input []*S) int {
+	sum := 0
+	for i := 0; i < len(input); i++ {
+		sum += input[i].i
+	}
+
+	return sum
+}
--- benchmarks/ranges/main_test.go
+++ benchmarks/ranges/main_test.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"math/rand"
+	"math/rand/v2"
 	"testing"
-	"time"
 )
+
+const n = 1_000
 
 var (
 	global int
-	input  = getRandomElements()
+	inputV = getRandomVElements()
+	inputP = getRandomPElements()
 )
 
 func Benchmark_sumValueElements(b *testing.B) {
 	var local int
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		local = sumValueElements(input)
+		local = sumValueElements(inputV)
 	}
 	global = local
 }
@@ -24,18 +26,45 @@ func Benchmark_sumValuesByIndex(b *testing.B) {
 	var local int
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		local = sumValuesByIndex(input)
+		local = sumValuesByIndex(inputV)
 	}
 	global = local
 }
 
-func getRandomElements() []S {
-	n := 1_000
-	res := make([]S, n)
-	src := rand.NewSource(time.Now().UnixNano())
-	rnd := rand.New(src)
-	for i := 0; i < n; i++ {
-		res[i].i = rnd.Int()
+func Benchmark_sumPointerElements(b *testing.B) {
+	var local int
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		local = sumPointerElements(inputP)
 	}
-	return res
+	global = local
+}
+
+func Benchmark_sumPointersByIndex(b *testing.B) {
+	var local int
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		local = sumPointersByIndex(inputP)
+	}
+	global = local
+}
+
+// TODO: generics?
+func getRandomVElements() []S {
+	ret := make([]S, n)
+	for i := 0; i < n; i++ {
+		ret[i].i = rand.Int()
+	}
+
+	return ret
+}
+
+func getRandomPElements() []*S {
+	ret := make([]*S, 0, n)
+	for i := 0; i < n; i++ {
+		e := &S{i: rand.Int()}
+		ret = append(ret, e)
+	}
+
+	return ret
 }
